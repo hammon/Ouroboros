f21d7727ee7fb3c3be0eb29a55db9f7e81320365
hammon <michael.alexandrov@gmail.com>
2015-03-19 02:06:42 +0200

    add main tabs

--- src/main/resources/webStatic/js/layout.js
+++ src/main/resources/webStatic/js/layout.js
@@ -5,91 +5,62 @@ Ext.require(['*']);
 Ext.onReady(function(){
 
    Ext.create('Ext.Viewport',{
-       //title: 'FB',
-           layout: {
-               type: 'border',
-               padding: 5
-           },
-           defaults: {
-              collapsible: true,
-              split: true,
-             // bodyPadding: 10
-           },
-           items: [
-                         Ext.create('Ouroboros.SplitView',{
+       layout: {
+           type: 'border',
+           padding: 5
+       },
+       defaults: {
+          collapsible: true,
+          split: true
+       },
+       items: [
+        Ext.createWidget('tabpanel', {
+            id: 'mainTabs',
+            region: 'center',
+            collapsible: false,
+            activeTab: 0,
+            items: [
+                Ext.create('Ouroboros.SplitView',{
+                    region:'center',
+                    title: 'Files',
+                    items: [
+                        Ext.create('Ouroboros.SplitView',{
                             region:'west',
                             width: 325,
                             items: [
                                 Ext.create('Ouroboros.FilesTree',{
                                     region:'center',
-                                  //  floatable: false,
-                                  //  margin: '5 0 0 0',
-                                    //width: 225,
                                     minWidth: 100,
                                 }),
                                 Ext.create('Ouroboros.NgramsGrid',{
                                     region:'south',
                                     id:'ngramsGrid'
-                                //    floatable: false,
-                                 //   margin: '5 0 0 0',
-                                  //  width: 225,
-                                  //  minWidth: 100,
                                 })
                             ]
                          }),
-
                         Ext.create('Ouroboros.SplitView',{
                             region:'center',
                            // width: 225,
                             items: [
                             Ext.create('Ouroboros.SnippetsGrid',{
-                             region:'center',
-                             id: 'snippetsGrid'
-                            //  floatable: false,
-                            //  margin: '5 0 0 0',
-                             //width: 225,
-                            // minWidth: 100,
+                                region:'center',
+                                id: 'snippetsGrid'
                             }),
                             Ext.create('Ouroboros.TextView',{
                              region:'south',
                              id:'textView'
-                            //    floatable: false,
-                            //   margin: '5 0 0 0',
-                            //  width: 225,
-                            //  minWidth: 100,
                             })
                             ]
                         })
-
-
-//                        ,
-
-//                Ext.createWidget('tabpanel', {
-//                        id: 'mainTabs',
-//                        region: 'center',
-//                        collapsible: false,
-//                        activeTab: 0,
-////                        defaults :{
-////                            bodyPadding: 10
-////                        },
-//                        items: [
-////                        {
-////                            html: '<h2>Main Page</h2><p>This is where the main content would go</p>',
-////                            title: 'Short Text',
-////                            closable: true
-////                        },
-//                        {
-//                            html: '<h2>Environment configuration</h2><p>Wellcome!</p>',
-//                            title: 'Environment configuration'
-//                        }]
-//                    })
-           ]//,
-
-       });
-
-
-//loadEnvs();
-
+                    ]
+                }),
+                {
+                    html: '<h2>Web</h2><p>Wellcome!</p>',
+                    title: 'Web'
+                }]
+            })
+        ]
+   });
 });
 
 
